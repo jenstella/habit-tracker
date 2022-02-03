677f11a0ef9c5ffc3446847d3c72e46729ac7807
Jen Martelle <77863818+jenstella@users.noreply.github.com>
2022-02-03 13:30:28 -0500

    Footer images added

--- habit-tracker/src/components/Footer.jsx
+++ habit-tracker/src/components/Footer.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import '../Footer.css'
+import React from "react";
+import "../Footer.css";
 
 export default function Footer() {
-  
-    return (
-        <div id="footer">
-            <p>this is the footer</p>
-        </div>
-    )
-};
+  return (
+    <div id="footer">
+      {/* home button */}
+      <img
+        id="home"
+        className="image"
+        src="/src/icons/home.svg"
+        alt="home icon"
+      ></img>
+      {/* plus button */}
+      <img
+        id="plus"
+        className="image"
+        src="/src/icons/plus.svg"
+        alt="plus icon"
+      ></img>{" "}
+      {/* metrics button */}
+      <img
+        id="graph"
+        className="image"
+        src="/src/icons/graph.svg"
+        alt="graph icon"
+      ></img>
+    </div>
+  );
+}
--- habit-tracker/src/components/Header.jsx
+++ habit-tracker/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import '../Header.css'
+import "../Header.css";
 
 export default function Header() {
   return (
     <div id="header">
-        <img id="settings" src="/src/icons/settings.svg" alt="settings svg icon"></img>
+      <img
+        id="settings"
+        src="/src/icons/settings.svg"
+        alt="settings svg icon"
+      ></img>
       <h3>Habit Tracker</h3>
       <img id="filter" src="/src/icons/filter.svg" alt="filter icon"></img>
     </div>
