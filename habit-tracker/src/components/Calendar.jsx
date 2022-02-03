b438b4a90f3fe45e6877a7e3c8b1f23bd640aba0
Jen Martelle <77863818+jenstella@users.noreply.github.com>
2022-02-03 15:31:52 -0500

    calendar

--- habit-tracker/src/components/Calendar.jsx
+++ habit-tracker/src/components/Calendar.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React from "react";
+import "../Calendar.css";
+
 
 export default function Calendar() {
-  return <div>add calendar images</div>;
+  return (
+    <div>
+      <div id="calendar">
+        <p id="monday">MON<br></br>24</p>
+        <p id="tuesday">TUES<br></br>25</p>
+        <p id="weds">WEDS<br></br>26</p>
+        <p id="thurs">THURS<br></br>27</p>
+        <p id="fri">FRI<br></br>28</p>
+        <p id="sat">SAT<br></br>29</p>
+        <p id="sun">SUN<br></br>30</p>
+      </div>
+    </div>
+  );
 }
--- habit-tracker/src/components/Home.jsx
+++ habit-tracker/src/components/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Footer from "./Footer";
 import "../Home.css";
+import Calendar from "./Calendar";
 
 export default function Home() {
   return (
     <div>
       <div id="homeText">
-        <p>You have not set any habits yet.</p>
+          {/* <Calendar/> */}
+        <div id="tasks">
+            {/* put a ternary in here if no tasks ? show text : show */}
+          <p>You have not set any habits yet.</p>
+        </div>
       </div>
       <Footer />
     </div>
