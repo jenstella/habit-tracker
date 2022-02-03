d7ed36b025980dbd5b814d0bf276eef467d09fc9
Jen Martelle <77863818+jenstella@users.noreply.github.com>
2022-02-03 14:01:21 -0500

    home responsive

--- habit-tracker/src/components/Home.jsx
+++ habit-tracker/src/components/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Footer from './Footer';
+import React from "react";
+import Footer from "./Footer";
+import "../Home.css";
 
 export default function Home() {
-    return (
-        <div>
-            <p>You have not set any habits yet.</p>
-            <Footer/>
-        </div>
-
-    ) 
-};
+  return (
+    <div>
+      <div id="homeText">
+        <p>You have not set any habits yet.</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
