60282de7b1caea2f0d584d986c30b3d27dabfe91
Jen Martelle <77863818+jenstella@users.noreply.github.com>
2022-02-04 20:17:27 -0500

    habit header changes

--- habit-tracker/src/components/AddHabitHeader.jsx
+++ habit-tracker/src/components/AddHabitHeader.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import "../AddHabit.css";
-import {Button} from 'react-bootstrap';
+import "../HabitHeader.css";
+import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-
 
 export default function AddHabitHeader() {
   let navigate = useNavigate();
 
-
   return (
     <div id="header">
-      <form>
-        <Button variable="btn btn-link" onClick={() => navigate('/')}>
-      <p>Back</p>
-      </Button>
-      </form>      
-      <h3 id="header-text">Yoga Habit</h3>
+      <div id="back-button">
+        <form>
+          <Button variable="btn btn-link" onClick={() => navigate("/")}>
+            <p>Back</p>
+          </Button>
+        </form>
+      </div>
+      <div id="header-text">
+        <h3>Yoga Habit</h3>
+      </div>
     </div>
-  )
-};
+  );
+}
